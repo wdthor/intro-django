770fd76354dc25518864c770a672acefd214f447
wdthor <wdthor@proton.me>
2025-05-07 14:06:30 +0200

    Setup whitenoise and add build command

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+# Install dependencies
+pip install -r requirements.txt
+# Collect static files
+python manage.py collectstatic --no-input
+# Migrate database
+python manage.py migrate
--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
